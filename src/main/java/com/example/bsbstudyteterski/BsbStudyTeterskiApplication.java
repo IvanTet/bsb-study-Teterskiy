58f9f86718107de59a5630ff2d9568cc18140f3d
IvanTet <vanocmok@gmail.com>
2023-01-18 18:55:20 +0300

    initial commit

---
+++ src/main/java/com/example/bsbstudyteterski/BsbStudyTeterskiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bsbstudyteterski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsbStudyTeterskiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsbStudyTeterskiApplication.class, args);
+    }
+
+}
