150ff355779e5ac7a26fc1db332639dd87c0a4e8
IvanTet <vanocmok@gmail.com>
2023-01-19 16:39:23 +0300

    add DB liquibase migration

---
+++ src/test/java/com/example/bsbstudyteterski/BsbStudyTeterskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsbstudyteterski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsbStudyTeterskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
