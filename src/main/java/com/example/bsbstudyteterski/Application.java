8e19b5bfdd6fd7553e79fc50e42a715480940943
IvanTet <vanocmok@gmail.com>
2023-01-19 21:15:35 +0300

    change gitignore

--- src/main/java/com/example/bsbstudyteterski/Application.java
+++ src/main/java/com/example/bsbstudyteterski/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BsbStudyTeterskiApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BsbStudyTeterskiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
